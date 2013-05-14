2a7b2a7f04cc31090444f9501b5c49022a457b92
Jon McKenzie <jcmcken@gmail.com>
2013-05-14 09:20:31 -0400

    RpmManager now copies in createrepo cache dir and repodata dir from old repo into staged to speed up repo build

--- crate/managers/rpm.py
+++ crate/managers/rpm.py
@@ -1,6 +1,11 @@
 from crate.managers.file import FileManager
 from crate.fs import files_in_dir
 from crate.proc import run
+import shutil
+import os
+import logging
+
+LOG = logging.getLogger(__name__)
 
 class RpmManager(FileManager):
     def locate(self):
@@ -9,3 +14,20 @@ class RpmManager(FileManager):
     
     def build(self, destination):
         code, stdout, stderr = run('createrepo -c .cache .', cwd=destination)
+        LOG.debug(stdout)
+        if stderr:
+            LOG.error(stderr)
+
+    def stage(self, files):
+        tmpdir = FileManager.stage(self, files)
+
+        # copy in old repodata and createrepo cache if they exist
+        repodata = os.path.join(self.destination, 'repodata')
+        cache = os.path.join(self.destination, '.cache')
+        for d in 'repodata', '.cache':
+            full = os.path.join(self.destination, d)
+            if os.path.isdir(full):
+                LOG.debug('copying "%s" directory to staged repo' % full)
+                shutil.copytree(full, os.path.join(tmpdir, d))
+
+        return tmpdir
