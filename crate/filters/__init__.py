a94ca890a7aff320cfe747c637efbc44ecb6fa89
Jon McKenzie <jcmcken@gmail.com>
2013-05-14 11:59:17 -0400

    Add RpmLatestFilter

--- crate/filters/__init__.py
+++ crate/filters/__init__.py
@@ -1,10 +1,11 @@
-from crate.filters.rpm import RpmNameFilter, RpmArchFilter
 from crate.filters.core import BelongsToFilter, RegexFilter
+from crate.filters.rpm import RpmNameFilter, RpmArchFilter, RpmLatestFilter
 
 FILTERS = {
   'regex': RegexFilter,
   'belongs_to': BelongsToFilter,
   'rpm_name': RpmNameFilter,
   'rpm_arch': RpmArchFilter,
+  'rpm_latest': RpmLatestFilter,
 }
 
--- crate/filters/rpm.py
+++ crate/filters/rpm.py
@@ -1,4 +1,4 @@
-from crate.filters.core import Filter
+from crate.filters.core import Filter, BelongsToFilter
 from crate._rpm import get_header, compare_versions
 
 class RpmArchFilter(Filter):
@@ -8,3 +8,22 @@ class RpmArchFilter(Filter):
 class RpmNameFilter(Filter):
     def transform(self, item):
         return get_header(item)['name']
+
+class RpmLatestFilter(BelongsToFilter):
+    def build_args(self, items):
+        storage = {}
+
+        for item in items:
+            current_header = get_header(item)
+            rpm_name = current_header['name']
+    
+            if rpm_name not in storage:
+                storage[rpm_name] = (item, current_header)
+            else:
+                old_filename, old_header = storage[rpm_name]
+                comparison = compare_versions(old_header, current_header) 
+                if comparison > 0:
+                    storage[rpm_name] = (item, current_header)
+
+        self.args = [ v[0] for v in storage.values() ]
+
